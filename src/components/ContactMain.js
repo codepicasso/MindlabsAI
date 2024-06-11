8d15401bd3edc659e5a967b1ac67e7f5c1fc0ff9
Code Picasso <150630739+codepicasso@users.noreply.github.com>
2024-06-11 16:35:48 +0200

    Update ContactMain.js

--- src/components/ContactMain.js
+++ src/components/ContactMain.js
@@ -41,10 +41,7 @@ const ContactMain = () => {
             <div className='section-title mb-4 pb-2'>
               <h2 className='title'>Direct contact us? </h2>
               <p className='content mb-0'>
-                For your car we will do everything advice, repairs and
-                maintenance. We are the some preferred choice by many car owners
-                because our experience and knowledge is selfe vident.For your
-                car we will do som everything.
+                Need expert AI solutions? Reach out to us. With our advanced technology expertise and deep industry knowledge, we're the preferred partner for innovative AI development. Connect with us today for cutting-edge, reliable AI services.
               </p>
             </div>
             <form ref={form} onSubmit={sendEmail}>
@@ -113,7 +110,7 @@ const ContactMain = () => {
       <div className='contact-g-map'>
         <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=West%20Perth,%20Australia+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe>
       </div>
-      
+
       {/* ================= Contact Main End =================*/}
     </>
   );
